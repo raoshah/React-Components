185eb24e1d2c0f7d2d2b052661b55328806b6d70
raoshah <shahrukhrao1@gmail.com>
2025-05-04 07:25:54 +0530

    May4

--- myapp/src/components/navbar/Navbar.js
+++ myapp/src/components/navbar/Navbar.js
@@ -2,8 +2,22 @@ import './Navbar.css'
 
 
 const Navbar = () => {
-    return <nav>
-        hello nav
+    return <nav class="navbar">
+        <div class="logo">MyBrand</div>
+
+        <input type="checkbox" id="toggle" class="checkbox" />
+        <label for="toggle" class="hamburger">
+            <span></span>
+            <span></span>
+            <span></span>
+        </label>
+
+        <ul class="nav-links">
+            <li><a href="#">Home</a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Services</a></li>
+            <li><a href="#">Contact</a></li>
+        </ul>
     </nav>
 }
 
--- myapp/src/index.js
+++ myapp/src/index.js
@@ -5,7 +5,7 @@ import Navbar from './components/navbar/Navbar.js'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<>
-  <h1 className="title" >React Components</h1>
+  {/* <h1 className="title" >React Components</h1> */}
   <Navbar/>
 </>
 );
