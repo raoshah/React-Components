b3d1211cf29321fd2d9397e1cbb5e342dbaf1d06
raoshah <shahrukhrao1@gmail.com>
2025-05-13 21:12:47 +0530

    May 13

---
+++ myapp/src/components/footer/Footer.js
@@ -0,0 +1,9 @@
+import './Footer.css'
+
+const Footer = () => {
+    return <div className='footer'>
+     <p >Copyright &copy; 2025 My Company</p>
+    </div>
+}
+
+export default Footer;
--- myapp/src/index.js
+++ myapp/src/index.js
@@ -1,12 +1,17 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Navbar from './components/navbar/Navbar.js'
+import Navbar from './components/navbar/Navbar.js';
+import Footer from './components/footer/Footer.js';
+import Products from './components/productDisplay/Index.js'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<>
   {/* <h1 className="title" >React Components</h1> */}
   <Navbar/>
+  <div style={{"min-height":"90vh"}}>
+    <Products/>
+  </div>
+  <Footer/>
 </>
 );
 
